1e250110e68bb94d63244a46dab43745324e0c6c
Fujaš <jfujas@factcool.com>
2024-10-15 10:43:40 +0200

    upratovanie

--- app/UI/Animal/AnimalPresenter.php
+++ app/UI/Animal/AnimalPresenter.php
@@ -13,10 +13,6 @@ class AnimalPresenter extends Presenter
     {
     }
 
-    public function actionCreatePet()
-    {
-    }
-
     public function renderCreatePet()
     {
     }
@@ -32,26 +28,19 @@ class AnimalPresenter extends Presenter
         $this->template->action = 'update';
     }
 
-    public function renderUpdatePet(int $animalId)
-    {
-    }
 
     public function createComponentCreatePet(): Form
     {
         $form = new Form;
 
-        // Pridanie CSS triedy k celému formuláru
         $form->getElementPrototype()->class('styled-form');
 
-        // Skryté pole pre ID
         $form->addHidden('id', 'create');
 
-        // Pole pre meno
         $form->addText('name', 'Meno')
             ->setRequired()
             ->setHtmlAttribute('class', 'form-control');
 
-        // Kontajner pre kategóriu
         $categoryContainer = $form->addContainer('category');
 
         $categoryContainer->addText('id', 'Category Id:')
@@ -64,14 +53,12 @@ class AnimalPresenter extends Presenter
             ->setRequired()
             ->setHtmlAttribute('class', 'form-control');
 
-        // Kontajner pre tagy
         $tagContainer = $form->addContainer('tags');
 
         $tagContainer->addText('name', 'Tag Name:')
             ->setHtmlAttribute('placeholder', 'Tagy oddeľte čiarkami')
             ->setHtmlAttribute('class', 'form-control');
 
-        // Nahrávanie obrázkov
         $form->addUpload('photoUrls', 'Nahrávanie obrázkov')
             ->setHtmlAttribute('multiple', 'multiple') // Povolenie nahrávať viacero súborov
             ->addRule(Form::Image, 'Môžete nahrať iba obrázky.')
@@ -80,9 +67,6 @@ class AnimalPresenter extends Presenter
 
         $form->addCheckbox('removePhotoUrls', 'Vymazať aktuálne obrázky');
 
-
-
-        // Select pre status
         $form->addSelect('status', 'Status', [
             'available' => ApiPresenter::STATUS_AVAILABLE,
             'pending' => ApiPresenter::STATUS_PENDING,
@@ -91,7 +75,6 @@ class AnimalPresenter extends Presenter
             ->setRequired()
             ->setHtmlAttribute('class', 'form-control');
 
-        // Pridanie enctype pre formulár, aby podporoval nahrávanie súborov
         $form->getElementPrototype()->enctype = 'multipart/form-data';
 
 
--- readme.md
+++ readme.md
@@ -10,3 +10,5 @@ Id zvierat som riesil interne aby sa nemohli duplikovat a nedochadzalo k chybam.
 na pridanie novej vlastnosti treba pridat property do triedy Animal a dalej pridat field do formulara, kluc do JsonSerialize a metod pracujucich s xml.
 Obrazky sa nahravaju do adresara www/images
 
+--update : upravil som ulohu podla upraveneho zadania. Formular vykreslujem pomocou latte sablon a komunikacia s backendom sa deje javascriptom
+
