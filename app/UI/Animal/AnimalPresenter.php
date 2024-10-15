7a92a14cfb3d4b83c3dc172699b842e7678d05f2
Fujaš <jfujas@factcool.com>
2024-10-15 10:11:53 +0200

    upratovanie

--- app/UI/Animal/AnimalPresenter.php
+++ app/UI/Animal/AnimalPresenter.php
@@ -31,10 +31,10 @@ class AnimalPresenter extends Presenter
         parent::__construct();
     }
 
-    public function renderStatuses(): void
-    {
-        $this->template->statuses = self::ANIMAL_STATUSES;
-    }
+//    public function renderStatuses(): void
+//    {
+//        $this->template->statuses = self::ANIMAL_STATUSES;
+//    }
 
     public function renderAnimalsByStatus(): void
     {
@@ -54,26 +54,26 @@ class AnimalPresenter extends Presenter
     {
     }
 
-    public function renderSelectPetToDelete(): void
-    {
-        $animals = $this->xmlManager->readAnimalsFromFile();
-        $this->template->animals = $animals;
-        $this->template->action = 'delete';
-        $this->setView('selectPetToUpdate');
-
-    }
+//    public function renderSelectPetToDelete(): void
+//    {
+//        $animals = $this->xmlManager->readAnimalsFromFile();
+//        $this->template->animals = $animals;
+//        $this->template->action = 'delete';
+//        $this->setView('selectPetToUpdate');
+//
+//    }
 
     public function renderSelectPetToUpdate()
     {
-        $animals = $this->xmlManager->readAnimalsFromFile();
-        $this->template->animals = $animals;
+//        $animals = $this->xmlManager->readAnimalsFromFile();
+//        $this->template->animals = $animals;
         $this->template->action = 'update';
     }
 
-    public function actionUpdatePet(int $animalId): void
-    {
-        $this->getComponent('createPet')->addCheckbox('removePhotoUrls', 'Vymazať aktuálne obrázky');
-    }
+//    public function actionUpdatePet(int $animalId): void
+//    {
+//        $this->getComponent('createPet')->addCheckbox('removePhotoUrls', 'Vymazať aktuálne obrázky');
+//    }
 
 
 
@@ -84,48 +84,36 @@ class AnimalPresenter extends Presenter
             if ($animal->getId() === $animalId) {
                 $this->template->animal = $animal;
 
-//                $this->getComponent('createPet')->getComponent('name')->setValue($animal->getName());
                 $this->getComponent('createPet')->getComponent('id')->setValue($animal->getId());
-//                $this->getComponent('createPet')->getComponent('category')->getComponent('id')->setValue($animal->getCategory()->getId())->setHtmlAttribute('readonly', 'readonly');
-//                $this->getComponent('createPet')->getComponent('category')->getComponent('name')->setValue($animal->getCategory()->getName());
-//                $tags = $animal->getTags();
-//                foreach ($tags as $tag) {
-//                    $names[] = $tag->getName();
-//                }
-//                $tagsString = implode(',', $names);
-//                $this->getComponent('createPet')->getComponent('tags')->getComponent('name')->setValue($tagsString);
-//
-//                $this->getComponent('createPet')->getComponent('photoUrls')->setValue(implode(', ', $animal->getPhotoUrls()));
-
                 break;
             }
         }
-        $this->template->animal = $animal;
+//        $this->template->animal = $animal;
 
 
     }
 
 
-    public function actionDeletePet(int $animalId): void
-    {
-        try {
-            $success = $this->animalApiClient->deleteAnimal($animalId);
-            if ($success) {
-                $animal = $this->xmlManager->getAnimalById($animalId);
-                $undeletedFiles = $this->animalService->deleteImages($animal);
-                $this->xmlManager->deletePet($animalId);
-                if (count($undeletedFiles) > 0) {
-                    $this->flashMessage(sprintf( 'Tieto subory sa nepodarilo zmazat %s', implode(', ', $undeletedFiles)), 'warning');
-                }
-                $this->flashMessage('Zviera bolo úspešne zmazané.', 'success');
-            } else {
-                $this->flashMessage('Nepodarilo sa zmazať zviera.', 'error');
-            }
-        } catch (\Exception $e) {
-            $this->flashMessage('Nastala chyba pri mazaní zvieraťa: ' . $e->getMessage(), 'error');
-        }
-        $this->redirect('Home:default');
-    }
+//    public function actionDeletePet(int $animalId): void
+//    {
+//        try {
+//            $success = $this->animalApiClient->deleteAnimal($animalId);
+//            if ($success) {
+//                $animal = $this->xmlManager->getAnimalById($animalId);
+//                $undeletedFiles = $this->animalService->deleteImages($animal);
+//                $this->xmlManager->deletePet($animalId);
+//                if (count($undeletedFiles) > 0) {
+//                    $this->flashMessage(sprintf( 'Tieto subory sa nepodarilo zmazat %s', implode(', ', $undeletedFiles)), 'warning');
+//                }
+//                $this->flashMessage('Zviera bolo úspešne zmazané.', 'success');
+//            } else {
+//                $this->flashMessage('Nepodarilo sa zmazať zviera.', 'error');
+//            }
+//        } catch (\Exception $e) {
+//            $this->flashMessage('Nastala chyba pri mazaní zvieraťa: ' . $e->getMessage(), 'error');
+//        }
+//        $this->redirect('Home:default');
+//    }
 
     public function createComponentCreatePet(): Form
     {
@@ -189,10 +177,6 @@ class AnimalPresenter extends Presenter
         $form->addSubmit('send', 'Uložiť')
             ->setHtmlAttribute('class', 'btn btn-primary');
 
-
-        // Nastavenie callbacku po úspešnom submitnutí
-//        $form->onSuccess[] = $this->createPetSucceeded(...);
-
         return $form;
     }
 
@@ -202,67 +186,67 @@ class AnimalPresenter extends Presenter
         return $this->createComponentCreatePet();
     }
 
-    private function createPetSucceeded(Form $form): void
-    {
-
-        $values = (array) $form->getValues();
-
-        $tags = explode(',', $values['tags']['name']);
-
-        $tags = array_filter($tags, fn($tag) => trim($tag) !== '');
-        $tagsEntitiesArray = $this->animalService->processTags($tags);
-
-        $animal = (new Animal())
-            ->setName($values['name'])
-            ->setCategory((new Category())->setId((int) $values['category']['id'])->setName($values['category']['name']))
-            ->setStatus((string) $values['status'])
-            ->setTags($tagsEntitiesArray);
-
-//        ak sa zviera vytvara
-        if ($values['id'] === AnimalApiClient::ACTION_CREATE) {
-
-            $animal->setPhotoUrls($this->animalService->processPhotoUrls($values['photoUrls']));
-            $lowestAvailableId = $this->xmlManager->getLowestAvailableId();
-            $animal->setId($lowestAvailableId);
-            try {
-
-                $this->animalApiClient->createAnimal($animal);
-            } catch (\Exception $e) {
-                $this->flashMessage('Nepodarilo sa vytvorit zviera.', 'error');
-                return;
-            }
-            $this->xmlManager->writeToFile($animal);
-            $this->flashMessage('zviera uspesne vytvorene');
-
-//            ak sa zviera updatuje
-        } else {
-            $animal->setId($values['id']);
-            $currentStoredAnimal = $this->xmlManager->getAnimalById($animal->getId());
-            if (isset($values['removePhotoUrls']) && $values['removePhotoUrls']) {
-
-                $undeletedFiles = $this->animalService->deleteImages($currentStoredAnimal);
-                if (count($undeletedFiles) > 0) {
-                    $this->flashMessage(sprintf( 'Tieto subory sa nepodarilo zmazat %s', implode(', ', $undeletedFiles)), 'warning');
-                }
-
-            } else {
-                $animal->setPhotoUrls(array_merge($currentStoredAnimal->getPhotoUrls(), $this->animalService->processPhotoUrls($values['photoUrls'])));
-            }
-
-            try {
-
-                $this->animalApiClient->updateAnimal($animal);
-
-            } catch (\Exception $e) {
-                $this->flashMessage($e->getMessage(), 'error');
-                return;
-            }
-
-            $this->xmlManager->updateExisting($animal);
-            $this->flashMessage('zviera spesne updatnute');
-        }
-
-        $this->redirect('Home:default');
-    }
+//    private function createPetSucceeded(Form $form): void
+//    {
+//
+//        $values = (array) $form->getValues();
+//
+//        $tags = explode(',', $values['tags']['name']);
+//
+//        $tags = array_filter($tags, fn($tag) => trim($tag) !== '');
+//        $tagsEntitiesArray = $this->animalService->processTags($tags);
+//
+//        $animal = (new Animal())
+//            ->setName($values['name'])
+//            ->setCategory((new Category())->setId((int) $values['category']['id'])->setName($values['category']['name']))
+//            ->setStatus((string) $values['status'])
+//            ->setTags($tagsEntitiesArray);
+//
+////        ak sa zviera vytvara
+//        if ($values['id'] === AnimalApiClient::ACTION_CREATE) {
+//
+//            $animal->setPhotoUrls($this->animalService->processPhotoUrls($values['photoUrls']));
+//            $lowestAvailableId = $this->xmlManager->getLowestAvailableId();
+//            $animal->setId($lowestAvailableId);
+//            try {
+//
+//                $this->animalApiClient->createAnimal($animal);
+//            } catch (\Exception $e) {
+//                $this->flashMessage('Nepodarilo sa vytvorit zviera.', 'error');
+//                return;
+//            }
+//            $this->xmlManager->writeToFile($animal);
+//            $this->flashMessage('zviera uspesne vytvorene');
+//
+////            ak sa zviera updatuje
+//        } else {
+//            $animal->setId($values['id']);
+//            $currentStoredAnimal = $this->xmlManager->getAnimalById($animal->getId());
+//            if (isset($values['removePhotoUrls']) && $values['removePhotoUrls']) {
+//
+//                $undeletedFiles = $this->animalService->deleteImages($currentStoredAnimal);
+//                if (count($undeletedFiles) > 0) {
+//                    $this->flashMessage(sprintf( 'Tieto subory sa nepodarilo zmazat %s', implode(', ', $undeletedFiles)), 'warning');
+//                }
+//
+//            } else {
+//                $animal->setPhotoUrls(array_merge($currentStoredAnimal->getPhotoUrls(), $this->animalService->processPhotoUrls($values['photoUrls'])));
+//            }
+//
+//            try {
+//
+//                $this->animalApiClient->updateAnimal($animal);
+//
+//            } catch (\Exception $e) {
+//                $this->flashMessage($e->getMessage(), 'error');
+//                return;
+//            }
+//
+//            $this->xmlManager->updateExisting($animal);
+//            $this->flashMessage('zviera spesne updatnute');
+//        }
+//
+//        $this->redirect('Home:default');
+//    }
 
 }
